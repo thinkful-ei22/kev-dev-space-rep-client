7ca3bce19fcf124fce0aa9c190e0cf05b4f96a0a
dghero <devingreyhero@gmail.com>
2018-09-10 11:29:15 -0700

    Create basic landing page

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -4,23 +4,37 @@ import {Link, Redirect} from 'react-router-dom';
 
 import LoginForm from './login-form';
 
-export function LandingPage(props) {
-    // If we are logged in redirect straight to the user's dashboard
-    if (props.loggedIn) {
-        return <Redirect to="/dashboard" />;
+import './style/landing-page.css';
+
+export class LandingPage extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      isLoginShowing: false
+    };
+  }
+
+  render(){
+      // If we are logged in redirect straight to the user's dashboard
+      
+    if (this.loggedIn) {
+      return <Redirect to="/dashboard" />;
     }
 
     return (
-        <div className="home">
-            <h2>Welcome to Foo App</h2>
-            <LoginForm />
-            <Link to="/register">Register</Link>
-        </div>
+      <div className="home">
+        <h2>Welcome to Babble Tower!</h2>
+        <p>Ever wanted to learn a new language? Now you can! Babble Tower allows you to learn new languages with easy-to-use flashcard exercises! To get started, please login or register below!</p>
+        {/* <LoginForm /> */}
+        {/* <Link to="/register">Register</Link> */}
+      </div>
     );
 }
 
+}
+
 const mapStateToProps = state => ({
-    loggedIn: state.auth.currentUser !== null
+  loggedIn: state.auth.currentUser !== null
 });
 
 export default connect(mapStateToProps)(LandingPage);
