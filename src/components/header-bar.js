796cd988e590de650d05ff386ae82045fc9e7582
dghero <devingreyhero@gmail.com>
2018-09-14 14:33:05 -0700

    Replace <a> tags with <Link>s

--- src/components/header-bar.js
+++ src/components/header-bar.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {clearAuth} from '../actions/auth';
-import {clearAuthToken} from '../local-storage';
-import {Redirect} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import './style/header-bar.css';
 import LoginForm from './login-form';
 
@@ -16,13 +14,13 @@ export class HeaderBar extends React.Component {
     // Only render the log out button if we are logged in
     
     let navButtons = this.props.loggedIn
-      ? [ <a className='homeNav' href="/" key="home">Progress</a>,
-        <a className='learnNav' href='/learn' key="learn">Learn</a>]
-      : <a className='homeNav' href="/">Home</a>;
+      ? [ <Link className='homeNav' to="/" key="home">Progress</Link>,
+        <Link className='learnNav' to='/learn' key="learn">Learn</Link>]
+      : <Link className='homeNav' to="/">Home</Link>;
     
     let loginLogout = this.props.loggedIn
       ? [ <span className="userName" key="name">{'Hello, ' + this.props.user.name} </span>,
-        <a href="/logout" key="logout">Logout</a>]
+        <Link to="/logout" key="logout">Logout</Link>]
       : <LoginForm />;
 
   
